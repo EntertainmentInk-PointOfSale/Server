8e769308ac88c7525443a0eaa26289c9c90ce396
Dobly1 <45601392+Dobly1@users.noreply.github.com>
2022-09-12 15:11:15 -0400

    Develop data layer base

--- Server/Data_Access/data.js
+++ Server/Data_Access/data.js
@@ -1,17 +1,71 @@
 var mysql = require('mysql2');
 var data =  require('../../credentials.json');
+var queries = require('./Queries/creation_queries.json')
 
-var con = mysql.createConnection({
-    host : "localhost",
-    user : data.mysql_user,
-    password: data.mysql_pass
-})
+class Data {
 
-con.connect(function(err) {
-    if (err) {
-        console.log('err ' + err.stack);
-        return;
+    constructor() {
+
+        this.pool;
+
+        //Define inital connection used to construct DB
+        let con = mysql.createConnection({
+            host : "localhost",
+            user : data.mysql_user,
+            password: data.mysql_pass
+        })
+
+        con.connect(function(err) {
+            if (err) {
+                console.log('err ' + err.stack);
+                return;
+            }
+
+            console.log("Connected " + con)
+
+            console.log('Connected as id ' + con.threadId);
+        })
+
+        con.query(queries.create_db, function(error, results, fields) {
+            if(error) {
+                console.log(error);
+            }
+            console.log("Database succesfullly created");
+        })
+
+        con = mysql.createConnection({
+            host : "localhost",
+            user : data.mysql_user,
+            password: data.mysql_pass,
+            database : "EI_PLACE_DATA"
+        })
+
+        con.query(queries.create_stock_category, function(error, results, fields) {
+            if(error) {
+                console.log(error);
+            }
+            console.log("Stock Category succesfullly created");
+        })
+
+        con.query(queries.create_tax, function(error, results, fields) {
+            if(error) {
+                console.log(error);
+            }
+            console.log("Tax succesfullly created");
+        })
+
+
+
+       
     }
 
-    console.log('Connected as id ' + con.threadId);
-})
+}
+
+module.exports = {Data}
+
+
+
+
+
+
+
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const data = require('./Data_Access/data')
+//Data Initalization
+
+var DataAccess = new data.Data();
+
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!"});
