10316178ce11e67d8f851de8648f11efae674f51
Dobly1 <45601392+Dobly1@users.noreply.github.com>
2022-10-06 15:34:33 -0400

    Modified data access to use singlton module

--- Server/Data_Access/data.js
+++ Server/Data_Access/data.js
@@ -2,66 +2,109 @@ var mysql = require('mysql2');
 var data =  require('../../credentials.json');
 var queries = require('./Queries/creation_queries.json')
 
-class Data {
+//Data Singleton
+var myData = (function() {
+    'use strict';
 
-    constructor() {
+    var instance, _pool;
 
-        this.pool;
+    var _con = mysql.createConnection({
+        host : "localhost",
+        user : data.mysql_user,
+        password: data.mysql_pass
+    })
 
-        //Define inital connection used to construct DB
-        let con = mysql.createConnection({
-            host : "localhost",
-            user : data.mysql_user,
-            password: data.mysql_pass
-        })
-
-        con.connect(function(err) {
+    function _createTables() {
+        _con.connect(function(err) {
             if (err) {
                 console.log('err ' + err.stack);
                 return;
             }
 
-            console.log("Connected " + con)
+            console.log("Connected " + _con)
 
-            console.log('Connected as id ' + con.threadId);
+            console.log('Connected as id ' + _con.threadId);
         })
 
-        con.query(queries.create_db, function(error, results, fields) {
+        _con.query(queries.create_db, function(error, results, fields) {
             if(error) {
                 console.log(error);
             }
             console.log("Database succesfullly created");
         })
 
-        con = mysql.createConnection({
+        _con = mysql.createConnection({
             host : "localhost",
             user : data.mysql_user,
             password: data.mysql_pass,
             database : "EI_PLACE_DATA"
         })
 
-        con.query(queries.create_stock_category, function(error, results, fields) {
+        _con.query(queries.create_stock_category, function(error, results, fields) {
             if(error) {
                 console.log(error);
             }
             console.log("Stock Category succesfullly created");
         })
 
-        con.query(queries.create_tax, function(error, results, fields) {
+        _con.query(queries.create_tax, function(error, results, fields) {
             if(error) {
                 console.log(error);
             }
             console.log("Tax succesfullly created");
         })
 
+        _con.query(queries.create_supplier, function(error, results, fields) {
+            if(error) {
+                console.log(error);
+            }
+            console.log("Supplier succesfullly created");
+        })
 
+        _con.query(queries.create_stock, function(error, results, fields) {
+            if(error) {
+                console.log(error);
+            }
+            console.log("Stock succesfullly created");
+        })
 
-       
+        _pool = mysql.createPool({
+            host : "localhost",
+            user : data.mysql_user,
+            password: data.mysql_pass,
+            database : "EI_PLACE_DATA",
+            connectionLimit : 10,
+            queueLimit : 0
+        })
     }
 
-}
+    function _createInstance() {
+        var object = new Object();
 
-module.exports = {Data}
+        _createTables();
+
+
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if(!instance){
+                console.log("Made instance")
+                instance = _createInstance();
+            }
+            return instance;
+        },
+
+        getPool: function () {
+            return _pool;
+        }
+    }
+})();
+
+myData.getInstance();
+
+module.exports = {myData}
 
 
 
