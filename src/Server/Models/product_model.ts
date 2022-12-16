9b55e1750cd17c84fb8143db8bee5cbc9af04303
cmacleod <45601392+Dobly1@users.noreply.github.com>
2022-12-16 15:09:08 -0500

    Fix TypeScript packages and rearrange structure

---
+++ src/Server/Models/product_model.ts
@@ -0,0 +1,13 @@
+export type ProductModel = {
+    stock_code: string;
+    product_name: string;
+    selling_price: number;
+    purchase_price: number;
+    stock_level: number;
+    date_added: string;
+    stock_category: number;
+    reorder_level: number;
+    unit_cost: number;
+    items_unit: number;
+    supplier_id: number;
+};
