35e9cb5fb87ca84466ffdb1c3e34fca463d51dc3
calmacleod <cmcallmacleod@gmail.com>
2023-03-14 22:03:10 -0400

    Implement dirty middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,23 @@
 //@ts-nocheck
-const router = require('express').Router();
+import { LogManager } from "../LogManager";
 
+const router = require('express').Router();
 const stockcategory = require('./StockCategoryRouter')
 const taxcategory   = require('./TaxRouter')
 const paymentmade   = require('./PaymentMadeRouter')
 const paymentmethod = require('./PaymentMethodRouter')
 const transaction   = require('./TransactionRouter')
 const customer      = require('./CustomerRouter')
+const logger = LogManager.getLogger();
 
 router.get('/', (req,res) => {
     res.status(200).json({ message: 'Connected'})
+})
+
+//Log Requests
+router.use((req,res,next) => {
+    logger.info(req);
+    next();
 })
 
 router.use('/stockcategory', stockcategory)
