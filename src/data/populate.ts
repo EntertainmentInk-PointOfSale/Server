9ca664e9751023414064776fe51a826061de4a76
calmacleod <cmcallmacleod@gmail.com>
2023-07-25 11:14:26 -0400

    Configure default stock categories

--- src/data/populate.ts
+++ src/data/populate.ts
@@ -2,10 +2,12 @@ import { DataSource } from "typeorm";
 import { populate_product } from "./default_data/default.product";
 import { popuplate_supplier } from "./default_data/default.supplier";
 import {populate_tax} from "./default_data/default.tax";
+import { populate_category } from "./default_data/default.stock_category";
 
 
 export default async function load_defaults() {
     await populate_tax();
+    await populate_category();
     await popuplate_supplier();
     await populate_product();
 }
