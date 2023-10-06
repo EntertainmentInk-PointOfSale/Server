7b63aa95b3c391a90c2da91f3422c3764faeda10
calmacleod <cmcallmacleod@gmail.com>
2023-10-06 19:05:43 -0400

    Begin transaction route work

---
+++ src/controller/TransactionController.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction, RequestHandler} from 'express';
+
+export const postTransaction:RequestHandler = async (req:Request, res: Response, next:NextFunction) => {
+    // POST transaction
+
+    // Create new transaction item
+
+    // Add sale items
+
+    // Verify
+}
